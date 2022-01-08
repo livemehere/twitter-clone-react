1a67a050e5ddf8253ef3da5ca0a8ecd2ae22373e
kongtaemin <rhdxoals@gmail.com>
2022-01-09 00:31:06 +0900

    이미지 업로드 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,58 @@
 import { useEffect, useState } from "react";
 import firebaseInstance from "../firebaseInstance";
+import { v4 as uuidv4 } from "uuid";
 import {
   getFirestore,
   collection,
   addDoc,
-  getDocs,
   onSnapshot,
-  doc,
 } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 import Tweet from "../components/Tweet";
 
 function Home({ user }) {
   const [tweet, setTweet] = useState("");
   const db = getFirestore(firebaseInstance);
+  const storage = getStorage(firebaseInstance);
+  const [photo, setPhoto] = useState();
+  const [type, setType] = useState("");
+  const [isLoading, setIsLoading] = useState();
   const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
     getTweetsFromDB();
   }, []);
 
+  const upload = async (file, type, setLoading) => {
+    const fileRef = ref(storage, `/tweetPhotos/${uuidv4()}.${type}`); //FIXME: 이건 Png말고 각자의 확장자로 변경해주기
+    setLoading(true);
+    const snapshot = await uploadBytes(fileRef, file);
+    setLoading(false);
+    alert("업로드 성공");
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    addTweetToDB(tweet);
+    if (tweet.length <= 0) {
+      alert("공백을 채워주세요!");
+      return;
+    }
+
+    // addTweetToDB(tweet);
+
+    upload(photo, type, setIsLoading);
+    setPhoto(null);
+    setType("");
     setTweet("");
   };
   const onChange = (e) => {
     setTweet(e.target.value);
+  };
+  const onFileChange = (e) => {
+    if (e.target.files[0]) {
+      setPhoto(e.target.files[0]);
+    }
+    setType(e.target.files[0].type.replace(/image\//, ""));
   };
 
   const getTweetsFromDB = async () => {
@@ -62,8 +89,9 @@ function Home({ user }) {
           value={tweet}
           onChange={onChange}
           maxLength={100}
-          placeholder="What is on your mind?"
+          placeholder="What's happening'"
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" />
       </form>
       <div>
