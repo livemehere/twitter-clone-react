a7c440d5b1d0f3ca27607b04b86658f5400ad1fb
kongtaemin <rhdxoals@gmail.com>
2022-01-10 01:03:58 +0900

    Updates

---
+++ precache-manifest.214806e7de5f65d74952e27eacfbf665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6bc0ea6fd4cd1ac0b7b0700897c32a",
+    "url": "/twitter-clone-react/index.html"
+  },
+  {
+    "revision": "db2d00454051870ddd40",
+    "url": "/twitter-clone-react/static/css/main.5e37584c.chunk.css"
+  },
+  {
+    "revision": "d6a6e9941c51ac7b7454",
+    "url": "/twitter-clone-react/static/js/2.d051b89f.chunk.js"
+  },
+  {
+    "revision": "db2d00454051870ddd40",
+    "url": "/twitter-clone-react/static/js/main.a2c8c4e3.chunk.js"
+  },
+  {
+    "revision": "536dcc1fb8524e49e957",
+    "url": "/twitter-clone-react/static/js/runtime~main.4ac32c6a.js"
+  }
+]);
